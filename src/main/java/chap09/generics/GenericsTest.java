fef214443ba692df041c0b3190a9514cec18e2bb
Hyeon9124 <hjgazzx@naver.com>
2022-11-13 14:55:29 +0900

    add

--- src/main/java/chap09/generics/GenericsTest.java
+++ src/main/java/chap09/generics/GenericsTest.java
@@ -5,7 +5,7 @@ public class GenericsTest {
 
         //**[1] : Generics 사용하지 않을 경우**//
         SimpleBox sb = new SimpleBox();
-        sb.setObject(33); // 자동형 변환(promotion)이 일어나서 error 가 안뜸. Promotion : Integer => Object
+        sb.setObject(33); // 자동 형변환(promotion)이 일어나서 error 가 안뜸. Promotion : Integer => Object
         Integer n1 = (Integer) sb.getObject(); // Type Casting (명시적 형변환) : Object => Integer
         System.out.println(n1);
 
@@ -14,7 +14,7 @@ public class GenericsTest {
         System.out.println(s1);
 
 
-        //**[2] : Generics 사용할 경우**//
+        //**[2] : Generics 사용한 경우**//
         Box<Integer> gb = new Box<>(); // Type Inference (타입 추론), Generic Type 으로 만드는 것
         gb.setT(33);
         Integer n2 = gb.getT();
@@ -26,7 +26,7 @@ public class GenericsTest {
         System.out.println(s2);
 
 
-        //**[3] : Raw Type 사용할 경우**//   !!(사용 X)!!
+        //**[3] : Raw Type 사용한 경우**//   !!(사용 X)!!
         Box rawBox = new Box();
         rawBox.setT(22);
         Integer n3 = (Integer) rawBox.getT(); // Casting 해야함.
