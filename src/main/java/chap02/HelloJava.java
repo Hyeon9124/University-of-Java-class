81845b4f81d7c8126c5da6a953eaff0bf309055e
Hyeon9124 <hjgazzx@naver.com>
2022-12-01 15:14:46 +0900

    change

--- src/main/java/chap02/HelloJava.java
+++ src/main/java/chap02/HelloJava.java
@@ -1,6 +1,6 @@
-package chap02;
+package chap02; // package
 public class HelloJava { // 클래스 시작
-    public static void main(String[] args) { // 메서드 본체 시작
+    public static void main(String[] args) { // main method 시작
         System.out.println("Hello Java World!"); // 실행문은 세미콜론(;)으로 끝난다.
         // Hello Java World! 출력
     } // 메서드 끝
