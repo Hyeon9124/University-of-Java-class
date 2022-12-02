931a3ea4d4b84e6d0966bbfce4a831bed7b10bae
Hyeon9124 <hjgazzx@naver.com>
2022-12-03 03:06:38 +0900

    change

--- src/main/java/chap09/exceptions/MethodCallStackTest01.java
+++ src/main/java/chap09/exceptions/MethodCallStackTest01.java
@@ -7,6 +7,7 @@ public class MethodCallStackTest01 {
         // 예외 처리
         try { // 예외가 발생할 수 있는 실행문
             BufferedReader reader = new BufferedReader(new FileReader("sample.txt"));
+            // Project01에 있는 sample.txt 파일을 읽는다.
             // FileReader(String fileName)
             String str = reader.readLine(); // 문자열을 입력 받는 코드
             System.out.println(str);
@@ -26,9 +27,15 @@ public class MethodCallStackTest01 {
 // ctrl + alt + t : try catch 해야할 영역을 선택후 실행
 /*
  -RuntimeExceptions(Unchecked Exception)-
- IndexOutOfBoundsException, NullPointerException
+ 실행 예외 : 프로그램 실행 도중에 예외가 발생하면 JVM은 해당하는 실행 예외 객체를 생성한다.
+ 실행 예외는 컴파일러가 예외 처리 여부를 확인하지 않기 때문에 개발자가 예외 처리 코드의 추가 여부를 결정해야 한다.
+
+ 대표적인 실행 예외 : IndexOutOfBoundsException, NullPointerException 등
  */
+
 /*
  -CompileTimeException(Checked Exception)-
- InterruptedException, IOException
+ 일반 예외 : 컴파일러는 일반 예외가 발생할 가능성이 있는 프로그램을 발견하면 컴파일 오류를 발생시킨다.
+ 따라서 개발자는 이런 프로그램에서는 예외 처리 코드를 반드시 추가해야 한다.
+ 대표적인 예 : InterruptedException, IOException 등
  */
--- src/main/java/chap09/exceptions/MethodCallStackTest03.java
+++ src/main/java/chap09/exceptions/MethodCallStackTest03.java
@@ -6,29 +6,21 @@ public class MethodCallStackTest03 {
     public void go() throws IOException{
         // throws : 예외를 처리하지 않고 발생한 예외 객체를 다른 곳으로 떠넘긴다.
         // IOException : 데이터 읽기 같은 입출력 문제가 있을 때 발생
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("sample.txt"));
-            String str = br.readLine();
+        BufferedReader br = new BufferedReader(new FileReader("sample.txt"));
+        String str = br.readLine();
 
-            System.out.println(str);
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println(str);
+        br.close();
     }
     public void come() throws IOException{ // come() method에서 예외 떠넘기기 (main 함수로)
-        try {
-            go();
-        } catch (IOException e) {
-            throw new RuntimeException(e); // Unchecked Exception
-        }
+        go();
     }
     public static void main(String[] args) {
         // main 함수에서 try ~ catch로 예외처리
         try {
             new MethodCallStackTest03().come(); // main method에 새로운 Constructor를 통해서 new 연산자로 부른다.
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // printStackTrace(System.err) : Throwable 객체와 추적 정보를 화면에 출력한다.
         }
     }
 }
